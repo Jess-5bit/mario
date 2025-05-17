2373fa4b0b9cad43c27abf552349c60a31dbc107
Jess-5bit <jessica_nguyen1@outlook.com>
2025-05-17 16:22:10 +1000

    s

--- mariohtml5-master/Enjine/gameCanvas.js
+++ mariohtml5-master/Enjine/gameCanvas.js
@@ -6,26 +6,40 @@
 Enjine.GameCanvas = function() {
 	this.Canvas = null;
 	this.Context2D = null;
-    this.BackBuffer = null;
+	this.BackBuffer = null;
 	this.BackBufferContext2D = null;
 };
 
 Enjine.GameCanvas.prototype = {
-    Initialize: function(canvasId, resWidth, resHeight) {
+	Initialize: function(canvasId, resWidth, resHeight) {
 		this.Canvas = document.getElementById(canvasId);
 		this.Context2D = this.Canvas.getContext("2d");
+
+		this.Canvas.width = window.innerWidth;
+		this.Canvas.height = window.innerHeight;
+
 		this.BackBuffer = document.createElement("canvas");
 		this.BackBuffer.width = resWidth;
 		this.BackBuffer.height = resHeight;
 		this.BackBufferContext2D = this.BackBuffer.getContext("2d");
+
+		window.addEventListener("resize", () => {
+			this.Canvas.width = window.innerWidth;
+			this.Canvas.height = window.innerHeight;
+		});
 	},
-	
-    BeginDraw: function() {
-        this.BackBufferContext2D.clearRect(0, 0, this.BackBuffer.width, this.BackBuffer.height);
-        this.Context2D.clearRect(0, 0, this.Canvas.width, this.Canvas.height);
-    },
-    
-    EndDraw: function() {
-        this.Context2D.drawImage(this.BackBuffer, 0, 0, this.BackBuffer.width, this.BackBuffer.height, 0, 0, this.Canvas.width, this.Canvas.height);
-    }
+
+	BeginDraw: function() {
+		this.BackBufferContext2D.clearRect(0, 0, this.BackBuffer.width, this.BackBuffer.height);
+		this.Context2D.clearRect(0, 0, this.Canvas.width, this.Canvas.height);
+	},
+
+	EndDraw: function() {
+		this.Context2D.imageSmoothingEnabled = false;
+		this.Context2D.drawImage(
+			this.BackBuffer,
+			0, 0, this.BackBuffer.width, this.BackBuffer.height,
+			0, 0, this.Canvas.width, this.Canvas.height
+		);
+	}
 };
